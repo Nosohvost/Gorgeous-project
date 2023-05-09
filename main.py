323d5ba51c0e591caf9243102c027f6f77200009
Nosohvost <savva.gorelik@gmail.com>
2023-05-09 14:10:12 +0100

    Polishing, preparations to add interactive buttons
    1) Added placeholder
    2) Unified ipady and pady for all buttons;
    3) Min height & min width of the window
    4) added functions when buttons are pressed

--- main.py
+++ main.py
@@ -4,31 +4,55 @@ class MainApp(tk.Tk):
     def __init__(self, title='Fox spy', *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.resizable(width=False, height=False)
+        # Minimum width & height for the window
+        self.MIN_WIDTH = 150
+        self.MIN_HEIGHT = 150
+        self.minsize(self.MIN_WIDTH, self.MIN_HEIGHT)
+
+        self.resizable(width=True, height=True)
         self.title(title)
 
-        self.menu = MainMenu(self, relief='groove', borderwidth=5, padx=5, pady=5)
-        self.menu.grid(row=0, column=1, padx=20, pady=10)
-
-        #Placeholder for quick statistics tab
-        self.statisticsTab = Placeholder(self, width=200, height=400, bg='red')
-        self.statisticsTab.grid(row=0, column=0)
+        # Create main menu
+        self.mainMenu = MainMenu(self)
+        self.mainMenu.grid(row=0, column=0)
 
 
+    def open_statistics_menu(self):
+        pass
+
+    def open_video_player(self):
+        pass
+
+    def open_settings(self):
+        pass
+
+# Main menu in top left corner
 class MainMenu(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
 
-        #Creating buttons
-        self.plotButton = tk.Button(self, text='Show plot')
-        self.videosButton = tk.Button(self, text='Saved videos') 
-        self.settingsButton = tk.Button(self, text='Settings')
+        # Creating buttons
+        self.plotButton = tk.Button(self, text='Show plot', command=master.open_statistics_menu)
+        self.videosButton = tk.Button(self, text='Saved videos', command=master.open_video_player) 
+        self.settingsButton = tk.Button(self, text='Settings', command=master.open_settings)
 
-        self.plotButton.grid(row=0, column=0, ipady=2, pady=4)
-        self.videosButton.grid(row=1, column=0, ipady=2, pady=4)
-        self.settingsButton.grid(row=2, column=0, ipady=2, pady=4)
+        self.buttons_pady = 4
+        self.buttons_ipady = 2
+
+        self.plotButton.grid(row=0, column=0, ipady=self.buttons_ipady, pady=self.buttons_pady)
+        self.videosButton.grid(row=1, column=0, ipady=self.buttons_ipady, pady=self.buttons_pady)
+        self.settingsButton.grid(row=2, column=0, ipady=self.buttons_ipady, pady=self.buttons_pady)
+
+class PlotCreator(tk.Frame):
+    pass
+
+class SettingsMenu(tk.Frame):
+    pass
+
+class VideoPlayer(tk.Frame):
+    pass
         
-
+# Basic placeholder for features that aren't implemented yet
 class Placeholder(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
