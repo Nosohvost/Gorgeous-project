f3a6794232297d482c2aed0c382474638480847e
Nosohvost <savva.gorelik@gmail.com>
2023-05-13 22:26:47 +0100

    Progress bar added, next/previous video fixed
    added threading lib to fix next/previous videos. Rewritten functions for next/previous video
    
    placed progress bar into a separate class
    
    If there are no videos in the folder, the app will probably crash - might want to fix this bug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkVideoPlayer import TkinterVideo
 import os
+import threading
 
 class MainApp(tk.Tk):
     def __init__(self, title='Fox spy', *args, **kwargs):
@@ -16,7 +17,7 @@ class MainApp(tk.Tk):
 
         # Create main menu
         self.mainMenu = MainMenu(self)
-        self.mainMenu.grid(row=0, column=0)
+        self.mainMenu.grid(row=0, column=0, sticky='NE')
 
         # Current tab opened. By default is statistics tab
         self.currentTab = Placeholder(self, bg='red')
@@ -36,6 +37,7 @@ class MainApp(tk.Tk):
         self.currentTab.destroy()
         self.currentTab = Placeholder(self, bg='green')
         self.currentTab.grid(row=0, column=1)
+
 
 # Main menu in top left corner
 class MainMenu(tk.Frame):
@@ -67,8 +69,12 @@ class VideoPlayer(tk.Frame):
         self.video_index = 0
         self.videos_list = ["videos\\" + file for file in os.listdir(r'.\videos')]
 
+        self.video = TkinterVideo(self, height=1, width=1, scaled=True)
+        # Fix for a bug in the tkVideoPlayer library
+        self.video.bind("<<Loaded>>", lambda e: e.widget.config(width=640, height=480))
+        
         # Toolbar
-        self.progressBar = Placeholder(self, height=25, width=640, bg='red')
+        self.progressBar = ProgressBar(self, height=20, width=640, video=self.video, bg='black')
         self.toolBar = tk.Frame(self)
         self.pauseButton = tk.Button(self.toolBar, text='Pause', 
                                      command=self.pause_button_click)
@@ -87,7 +93,9 @@ class VideoPlayer(tk.Frame):
         self.pauseButton.grid(row=0, column=1, padx=padx)
         self.nextVideoButton.grid(row=0, column=2, padx=padx)
 
-        self.load_video()
+        self.video.grid(row=0, column=0)
+        self.video.load(self.videos_list[0])
+        self.video.play()
 
     # Called when (un)pause button is clicked
     def pause_button_click(self):
@@ -99,25 +107,11 @@ class VideoPlayer(tk.Frame):
             self.video.play()
             self.pauseButton.config(text='Pause')
 
-    # Creates a new instance of TkinterVideo and deletes the previous one
+    # Loads video using self.video_index
     def load_video(self):
-        path = self.videos_list[self.video_index]
-        
-        try:
-            self.video.destroy()
-        except:
-            # Video is already closed
-            pass
-
-        self.video = TkinterVideo(self, height=1, width=1, scaled=True)
-
-        # Fix for a bug in the tkVideoPlayer library
-        self.video.bind("<<Loaded>>", lambda e: e.widget.config(width=640, height=480))
-
-        self.video.load(path)
-        self.video.grid(row=0, column=0)
-        self.video.play()
-        
+        self.video.load(self.videos_list[self.video_index])
+        timer = threading.Timer(0.1, lambda: self.video.play()) # Waits a bit to ensure previous video was closed
+        timer.start()
         self.pauseButton.config(text='Pause')
 
     # Load next video
@@ -125,13 +119,51 @@ class VideoPlayer(tk.Frame):
         self.video_index = min(self.video_index + 1, len(self.videos_list) - 1)
         self.load_video()
         
-
-
+        
     # Load previous video
     def previous_video(self):
         self.video_index = max(self.video_index - 1, 0)
         self.load_video()
     
+class ProgressBar(tk.Frame):
+    def __init__(self, master, height, width, video: TkinterVideo, *args, **kwargs):
+        super().__init__(master, height=height, width=width, *args, **kwargs)
+
+        self.height = height
+        self.width = width
+        
+        self.redLine = tk.Frame(self, height=self.height * 0.3, bg='red')
+        self.duration = 0
+        self.video = video
+        self.video.bind('<<Duration>>', self.set_duration)
+        self.video.bind('<<SecondChanged>>', self.update)
+        self.video.bind('<<Ended>>', self.video_ended)
+
+        self.pack_propagate(False) # Prevent shrinking the progress bar to fit the redLine
+        self.redLine.pack(side='left')
+
+    # Fix for a bug in TkinterVideo libary
+    def video_ended(self, event):
+        self.redLine.config(width=self.width)
+
+    # Update the total duration when a new video is loaded
+    def set_duration(self, event):
+        self.duration = self.video.video_info()['duration']
+        self.redLine.config(width=0)
+        
+    # Update the red line to match the current progress
+    def update(self, event):
+        if (self.duration != 0):
+            progress = self.video.current_duration() / self.duration # Between 0 and 1
+            #print(self.video.current_duration(), self.duration)
+        else:
+            progress = 0
+        new_width = int(self.width * progress)
+        self.redLine.config(width=new_width)
+
+
+
+
 
 # Basic placeholder for features that aren't implemented yet
 class Placeholder(tk.Frame):
