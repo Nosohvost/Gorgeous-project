1f6fee8fd2fbc6ab73073a47fff66acfd81f9497
Savva Gorelik <savva.gorelik@gmail.com>
2023-09-07 18:02:08 +0100

    Semi-working database; created __init__.py
    __init__.py allows to import files in src folder. Database works, but needs "delete all" function

--- src/dbutils.py
+++ src/dbutils.py
@@ -1,4 +1,35 @@
+import csv
+import threading
+
+DATABASE_PATH = './database.csv'
+
 # Stores records of foxes and other animals
 class Database():
     def __init__(self):
-        pass
+        self.lock = threading.Lock()
+
+    # Save a record into the end of csv file
+    def write_record(self, record: list):
+        # Acquire the lock to prevent a race condition and open the database
+        with self.lock, open(DATABASE_PATH, 'a', newline='') as file: 
+            writer = csv.writer(file, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(record)
+
+    # Get a list of all records
+    def read_records(self):
+        records = []
+        # Acquire the lock to prevent a race condition and open the database
+        with self.lock, open(DATABASE_PATH, 'r', newline='') as file:
+            reader = csv.reader(file, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+            for row in reader:
+                records.append(row)
+        return records
+
+if __name__ == "__main__":
+    DATABASE_PATH = '../database.csv'
+    db = Database()
+    db.write_record(['first', 'second', 345])
+    db.write_record([''])
+    db.write_record(['''this is a loooooooooooong sentence!'''])
+
+    print(db.read_records())
